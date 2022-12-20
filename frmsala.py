d025cb8c457d6769b8519c8a2ada6bf0803a4f46
Naes93 <107157371+Naes93@users.noreply.github.com>
2022-12-19 21:04:58 -0300

    frmSala

--- frmsala.py
+++ frmsala.py
@@ -1,110 +1,104 @@
-import tkinter as tk
+from tkinter import *
+
 import tkinter.font as tkFont
 
-class Sala(tk.Toplevel):
-    def __init__(self, master = None, sala_id =  None):
+import tkinter.ttk as ttk
+import tkinter.messagebox as tkMsgBox
+import bll.salas as rooms
+from frmsala import Sala
+
+class Salas(Toplevel):
+    def __init__(self, master=None):
         super().__init__(master)
-        self.master = master
-        self.sala_id = sala_id
-        #setting title
-        self.title("Registro de Sala")
-        #setting window size
-        width=267
-        height=186
+        self.select_id = -1
+        self.master = master        
+        self.title("Salas")        
+        width=670
+        height=350
         screenwidth = self.winfo_screenwidth()
         screenheight = self.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         self.geometry(alignstr)
         self.resizable(width=False, height=False)
 
-        GLabel_986=tk.Label(self)
+        GLabel_464=Label(self)
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_986["font"] = ft
-        GLabel_986["fg"] = "#333333"
-        GLabel_986["justify"] = "left"
-        GLabel_986["text"] = "Número:"
-        GLabel_986.place(x=10,y=10,width=70,height=25)
+        GLabel_464["font"] = ft
+        GLabel_464["fg"] = "#333333"
+        GLabel_464["justify"] = "center"
+        GLabel_464["text"] = "Usuarios:"
+        GLabel_464.place(x=10,y=10,width=70,height=25)
 
-        GLabel_949=tk.Label(self)
+        tv = ttk.Treeview(self, columns=("nombre", "tipo", "capacidad"), name="tvSalas")
+        tv.column("#0", width=50)
+        tv.column("nombre", width=100, anchor=CENTER)
+        tv.column("tipo", width=100, anchor=CENTER)
+        tv.column("capacidad", width=90, anchor=CENTER)
+
+        tv.heading("#0", text="Id", anchor=CENTER)
+        tv.heading("nombre", text="Nombre", anchor=CENTER)
+        tv.heading("tipo", text="tipo", anchor=CENTER)
+        tv.heading("capacidad", text="capacidad", anchor=CENTER)
+        tv.bind("<<TreeviewSelect>>", self.obtener_fila)          
+        
+        self.refrescar()
+
         ft = tkFont.Font(family='Times',size=10)
-        GLabel_949["font"] = ft
-        GLabel_949["fg"] = "#333333"
-        GLabel_949["justify"] = "left"
-        GLabel_949["text"] = "Formato:"
-        GLabel_949.place(x=10,y=50,width=70,height=25)
+        btn_agregar = Button(self)
+        btn_agregar["bg"] = "#f0f0f0"        
+        btn_agregar["font"] = ft
+        btn_agregar["fg"] = "#000000"
+        btn_agregar["justify"] = "center"
+        btn_agregar["text"] = "Agregar"
+        btn_agregar.place(x=430,y=10,width=70,height=25)
+        btn_agregar["command"] = self.agregar
 
-        GLabel_205=tk.Label(self)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_205["font"] = ft
-        GLabel_205["fg"] = "#333333"
-        GLabel_205["justify"] = "left"
-        GLabel_205["text"] = "Capacidad:"
-        GLabel_205.place(x=10,y=90,width=70,height=25)
+        btn_editar = Button(self)
+        btn_editar["bg"] = "#f0f0f0"        
+        btn_editar["font"] = ft
+        btn_editar["fg"] = "#000000"
+        btn_editar["justify"] = "center"
+        btn_editar["text"] = "Editar"
+        btn_editar.place(x=510,y=10,width=70,height=25)
+        btn_editar["command"] = self.editar
+        
+        btn_eliminar = Button(self)
+        btn_eliminar["bg"] = "#f0f0f0"        
+        btn_eliminar["font"] = ft
+        btn_eliminar["fg"] = "#000000"
+        btn_eliminar["justify"] = "center"
+        btn_eliminar["text"] = "Eliminar"
+        btn_eliminar.place(x=590,y=10,width=70,height=25)
+        btn_eliminar["command"] = self.eliminar
 
-        GLineEdit_119=tk.Entry(self, name = "txtNumero")
-        GLineEdit_119["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_119["font"] = ft
-        GLineEdit_119["fg"] = "#333333"
-        GLineEdit_119["justify"] = "left"
-        GLineEdit_119["text"] = ""
-        GLineEdit_119.place(x=80,y=10,width=112,height=30)
-
-        GLineEdit_522=tk.Entry(self, name = "txtFormato")
-        GLineEdit_522["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_522["font"] = ft
-        GLineEdit_522["fg"] = "#333333"
-        GLineEdit_522["justify"] = "left"
-        GLineEdit_522["text"] = ""
-        GLineEdit_522.place(x=80,y=50,width=111,height=30)
-
-        GLineEdit_557=tk.Entry(self, name = "txtCapacidad")
-        GLineEdit_557["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Times',size=10)
-        GLineEdit_557["font"] = ft
-        GLineEdit_557["fg"] = "#333333"
-        GLineEdit_557["justify"] = "left"
-        GLineEdit_557["text"] = ""
-        GLineEdit_557.place(x=80,y=90,width=111,height=30)
-
-        GButton_215=tk.Button(self)
-        GButton_215["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_215["font"] = ft
-        GButton_215["fg"] = "#000000"
-        GButton_215["justify"] = "center"
-        GButton_215["text"] = "Aceptar"
-        GButton_215.place(x=100,y=140,width=70,height=25)
-        GButton_215["command"] = self.aceptar
-
-        GButton_901=tk.Button(self)
-        GButton_901["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_901["font"] = ft
-        GButton_901["fg"] = "#000000"
-        GButton_901["justify"] = "center"
-        GButton_901["text"] = "Cancelar"
-        GButton_901.place(x=180,y=140,width=70,height=25)
-        GButton_901["command"] = self.cancelar
-
-    def aceptar(self):
-        numero = self.get_value("txtNumero")
-        formato = self.get_value("txtFormato")
-        capacidad = self.get_value("txtCapacidad")
-        print(numero, formato, capacidad)
-        #validar datos
-        #fijarme si es una alta o es una edicion
-        if self.sala_id is None:
-            print("alta nueva de sala")
-            #guardar los datos en la bd
+    def obtener_fila(self, event):
+        tvSalas = self.nametowidget("tvSalas")
+        current_item = tvSalas.focus()
+        if current_item:
+            data = tvSalas.item(current_item)
+            self.select_id = int(data["text"])
         else:
-            print("edicion de sala")
-            #guardar los datos en la bd       
+            self.select_id = -1
 
+    def agregar(self):
+        Sala(self, True)
+        print('agregar')
 
-    def cancelar(self):
-        self.destroy()
+    def editar(self): 
+        Sala(self, True, self.select_id)
 
-    def get_value(self, name):
-        return self.nametowidget(name).get()
+    def eliminar(self):
+        answer =  tkMsgBox.askokcancel(self.master.master.title(), "¿Está seguro de eliminar esta sala?")   
+        if answer:
+            rooms.eliminar(self.select_id)
+            self.refrescar()
+
+    # https://www.youtube.com/watch?v=n0usdtoU5cE
+    def refrescar(self):        
+        tvSalas = self.nametowidget("tvSalas")
+        for record in tvSalas.get_children():
+            tvSalas.delete(record)
+        salas = rooms.listar()
+        for room in salas:
+            tvSalas.insert("", END, text=room[0], values=(room[1], room[2], room[3]))        
+        tvSalas.place(x=10,y=40,width=650,height=300)
